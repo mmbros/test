a7d1b0e81eae10d475f1c43c9a9c0305d09fbe39
mmbros <server.mmbros@yandex.com>
2019-02-14 09:42:22 +0100

    https server

---
+++ tls-server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	// "fmt"
+	// "io"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// HelloServer is ...
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte("This is an example server.\n"))
+	// fmt.Fprintf(w, "This is an example server.\n")
+	// io.WriteString(w, "This is an example server.\n")
+
+}
+
+func main() {
+	port := ":4443"
+	http.HandleFunc("/", HelloServer)
+	fmt.Printf("Listening on %s ...\n", port)
+	err := http.ListenAndServeTLS(port, "localhost.cert.pem", "localhost.key.pem", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
